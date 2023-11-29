1ae3325226ab1ddce901132f8db131341cc383cf
Alexander Levin <alevin.ru@gmail.com>
2023-11-29 19:54:28 +0200

    Update predicates.ts

--- src/predicates.ts
+++ src/predicates.ts
@@ -17,6 +17,8 @@ type WhereType = Record<string, WhereClause>
 
 export function whereToFilter(where: WhereType, schema: Record<string, any> = {}) {
 
+  const keys: Record<string, any> = schema ? (schema.tree || schema) : {};
+
   return lo.mapValues(where, (value, name) => {
 
     const simpleFilter = getTypedValue(WhereOperator.EQ)
@@ -45,7 +47,7 @@ export function whereToFilter(where: WhereType, schema: Record<string, any> = {}
     return lo.pickBy(params, v => v !== undefined);
 
     function getTypedValue(path: WhereOperator) {
-      const key = schema[name];
+      const key = keys[name];
       const raw = lo.get(value, path);
       try {
         return (raw && key === Date) ? new Date(raw) : raw;
@@ -59,10 +61,11 @@ export function whereToFilter(where: WhereType, schema: Record<string, any> = {}
 }
 
 export function queryToFilter(query?: Record<string, any>, schema: Record<string, any> = {}): Record<string, any> {
+  const tree: Record<string, any> = schema.tree || schema || {};
   const queryKeys = Object.keys(query || {})
     .filter(name => {
       const [field] = name.match(/^[^.]+/) || [];
-      return field && schema[field];
+      return field && tree[field];
     });
   return lo.mapValues(lo.pick(query, queryKeys), x => {
     if (Array.isArray(x)) {
